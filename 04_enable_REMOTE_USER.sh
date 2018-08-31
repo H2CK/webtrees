9d98a3dfce73b61bbf5f514cc6907e6d33909e53
H2CK <H2CK@users.noreply.github.com>
2018-08-31 11:42:02 +0200

    fixed comparison bug

--- 04_enable_REMOTE_USER.sh
+++ 04_enable_REMOTE_USER.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 USE_REMOTE_USER=${ENABLE_REMOTE_USER:-FALSE}
-if [ "$USE_REMOTE_USER" = "TRUE"]
+if [ "$USE_REMOTE_USER" = "TRUE" ]
 then
 copy -f /Auth.php /var/www/html/app/Auth.php
 fi
