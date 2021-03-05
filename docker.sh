0d42f8273b18ee8d9f204a8ef4aba76d5956013e
H2CK <t@jagel.net>
2021-03-05 22:18:10 +0100

    Optimized Pipeline

--- docker.sh
+++ docker.sh
@@ -145,8 +145,19 @@ function docker_manifest_list_test_beta_latest() {
 
 function setup_dependencies() {
   echo "PREPARE: Setting up dependencies."
-  sudo apt update -y
-  sudo apt install --only-upgrade docker-ce -y
+  sudo apt-get remove docker docker-engine docker.io containerd runc
+  sudo apt-get update -y
+  sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg -y
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update -y
+  sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 }
 
 function update_docker_configuration() {
